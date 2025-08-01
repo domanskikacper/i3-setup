76448478f6edb9fcb3dd430518da8af28fe4a8a9
Kacper Domański <domanskikacper@icloud.com>
2025-08-01 19:28:52 +0200

    Update wifi_status.sh

--- polybar/scripts/wifi_status.sh
+++ polybar/scripts/wifi_status.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
 PRIMARY="#49D196"
+GRAY="#707880"
+YELLOW="#FFD700"
+RED="#FF0000"
 
 # Pobierz nazwę interfejsu Wi-Fi (pierwszy interfejs typu wifi)
 WIFI_DEV=$(nmcli device status | awk '$2=="wifi" {print $1; exit}')
 
 if [ -z "$WIFI_DEV" ]; then
     ICON="󱚾"
-    echo "%{F$PRIMARY}$ICON%{F-} N/A"
+    echo "%{F$GRAY}$ICON%{F-}"
     exit 0
 fi
 
 # Pobierz status połączenia
 STATUS=$(nmcli device status | awk -v dev="$WIFI_DEV" '$1==dev {print $3}')
 
-# Mapowanie statusów na ikony Nerd Font
+# Mapowanie statusów na ikony i kolory
 case "$STATUS" in
     connected)
         ICON="󰖩"
+        COLOR="$PRIMARY"
         ;;
     disconnected)
         ICON="󰖪"
+        COLOR="$GRAY"
         ;;
     connecting)
-        ICON="󱚾"
+        ICON="󱛄"
+        COLOR="$YELLOW"
         ;;
     unavailable)
-        ICON="󱛅"
+        ICON="󱚼"
+        COLOR="$GRAY"
         ;;
-    failed | *)
+    failed)
+        ICON="󱛅"
+        COLOR="$RED"
+        ;;
+    *)
         ICON="󱚵"
+        COLOR="$GRAY"
         ;;
 esac
 
-# Zwróć wynik z ikoną w kolorze i statusem jako tekst
-echo "%{F$PRIMARY}$ICON%{F-} $STATUS"
+# Zwróć wynik z ikoną w odpowiednim kolorze
+echo "%{F$COLOR}$ICON%{F-}"
